7937a98ae603d3ef4f5a22053fef8ae0843bc4a5
anshusinha872 <anshusinha872@gmail.com>
2022-01-29 18:53:35 +0530

    efficient approach

--- 4.cpp
+++ 4.cpp
@@ -21,58 +21,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2){
-    priority_queue<int> max_heap;
-    priority_queue<int, vector<int>, greater<>> min_heap;
-    int i = 0, j = 0;
-    vector<int> merge_vector;
-    while(i<nums1.size() && j<nums2.size()){
-        if(nums1[i]<nums2[j]){
-            merge_vector.push_back(nums1[i]);
-            i++;
-        }
-        else{
-            merge_vector.push_back(nums2[j]);
-            j++;
-        }
-    }
-    while(i<nums1.size()){
-        merge_vector.push_back(nums1[i]);
-        i++;
-    }
-    while(j<nums2.size()){
-        merge_vector.push_back(nums2[j]);
-        j++;
-    }
-    cout << "merged vector : ";
-    for(auto x:merge_vector)
-        cout << x << " ";
-    cout << endl;
-    max_heap.push(merge_vector[0]);
-    for (i = 1; i < merge_vector.size();i++){
-        int x = merge_vector[i];
-        if(max_heap.size()>min_heap.size()){
-            if(max_heap.top()>x){
-                min_heap.push(max_heap.top());
-                max_heap.pop();
-                max_heap.push(x);
+    // priority_queue<int> max_heap;
+    // priority_queue<int, vector<int>, greater<>> min_heap;
+    // int i = 0, j = 0;
+    // vector<int> merge_vector;
+    // while(i<nums1.size() && j<nums2.size()){
+    //     if(nums1[i]<nums2[j]){
+    //         merge_vector.push_back(nums1[i]);
+    //         i++;
+    //     }
+    //     else{
+    //         merge_vector.push_back(nums2[j]);
+    //         j++;
+    //     }
+    // }
+    // while(i<nums1.size()){
+    //     merge_vector.push_back(nums1[i]);
+    //     i++;
+    // }
+    // while(j<nums2.size()){
+    //     merge_vector.push_back(nums2[j]);
+    //     j++;
+    // }
+    // cout << "merged vector : ";
+    // for(auto x:merge_vector)
+    //     cout << x << " ";
+    // cout << endl;
+    // max_heap.push(merge_vector[0]);
+    // for (i = 1; i < merge_vector.size();i++){
+    //     int x = merge_vector[i];
+    //     if(max_heap.size()>min_heap.size()){
+    //         if(max_heap.top()>x){
+    //             min_heap.push(max_heap.top());
+    //             max_heap.pop();
+    //             max_heap.push(x);
+    //         }
+    //         else{
+    //             min_heap.push(x);
+    //         }
+    //     }
+    //     else{
+    //         if(x<=max_heap.top())
+    //             max_heap.push(x);
+    //         else{
+    //             min_heap.push(x);
+    //             max_heap.push(min_heap.top());
+    //             min_heap.pop();
+    //         }
+    //     }
+    // }
+    // if(max_heap.size()>min_heap.size())
+    //     return max_heap.top();
+    // return (max_heap.top() + min_heap.top()) / 2.0;
+
+    // using heap cause time complexity of O((m+n)log(m+n))
+    // efficient approach
+    // using binary search we can reduce time complexity to log(n) where n is size of smallar of both the arrays
+
+    // we do binary search for smaller size array
+    // we flip the array if nums2 array is smaller than nums1
+    if(nums2.size()<nums1.size())
+        return findMedianSortedArrays(nums2,nums1);
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+    int low = 0, high = n1;
+
+    while(low<=high){
+        int cut1 = (low + high)/2;
+        int cut2 = (n1 + n2 + 1)/2-cut1;
+
+        int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
+        int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
+
+        int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+        int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+        if(left1<=right2 && left2<=right1){
+            if((n1+n2)%2==0){
+                return (max(left2, left1) + min(right1, right2)) / 2.0;
             }
-            else{
-                min_heap.push(x);
-            }
+            else
+                return max(left1, left2);
         }
-        else{
-            if(x<=max_heap.top())
-                max_heap.push(x);
-            else{
-                min_heap.push(x);
-                max_heap.push(min_heap.top());
-                min_heap.pop();
-            }
-        }
+        else if(left1>right2)
+            high = cut1 - 1;
+        else
+            low = cut1 + 1;
     }
-    if(max_heap.size()>min_heap.size())
-        return max_heap.top();
-    return (max_heap.top() + min_heap.top()) / 2.0;
+    return 0.0;
 }
 int main(){
     int m, n;
