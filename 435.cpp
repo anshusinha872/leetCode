327dae6bb444b794324cd2d20c2a83eac1bb4e16
anshusinha872 <anshusinha872@gmail.com>
2022-03-07 16:48:32 +0530

    435. Non - overlapping Intervals

---
+++ 435.cpp
@@ -0,0 +1,48 @@
+// 435. Non - overlapping Intervals
+// Given an array of intervals intervals where intervals[i] = [ starti, endi ], return the minimum number of intervals you need to remove to make the rest of the intervals non - overlapping.
+
+//                                                                                                                                                                                Example 1 :
+
+//     Input : intervals = [ [ 1, 2 ], [ 2, 3 ], [ 3, 4 ], [ 1, 3 ] ] Output : 1 Explanation : [ 1, 3 ] can be removed and the rest of the intervals are non - overlapping.Example 2 :
+
+//     Input : intervals = [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ] ] Output : 2 Explanation : You need to remove two[1, 2] to make the rest of the intervals non - overlapping.Example 3 :
+
+//     Input : intervals = [ [ 1, 2 ], [ 2, 3 ] ] Output : 0 Explanation : You don 't need to remove any of the intervals since they' re already non - overlapping.
+
+#include<bits/stdc++.h>
+using namespace std;
+int eraseOverlapIntervals(vector<vector<int>> &intervals){
+    sort(intervals.begin(), intervals.end());
+    int count = 0;
+    int left = 0, right = 1;
+    int n = intervals.size();
+    while(right<n){
+        if(intervals[left][1]<=intervals[right][0]){
+            left = right;
+            right += 1;
+        }
+        else if(intervals[left][1]<=intervals[right][1]){
+            count += 1;
+            right += 1;
+        }
+        else if(intervals[left][1]>intervals[right][1]){
+            count += 1;
+            left = right;
+            right += 1;
+        }
+    }
+    return count;
+}
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> interval;
+    for (int i = 0; i < n; i++)
+    {
+        int si, ei;
+        cin >> si >> ei;
+        interval.push_back({si, ei});
+    }
+    cout << eraseOverlapIntervals(interval);
+    return 0;
+}
